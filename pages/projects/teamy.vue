4c4ac190efe44279502e6ae7bab84007aa62ce35
Lesti Dániel <daniellesti73@gmail.com>
2020-12-14 09:17:10 +0100

    Meow

--- pages/projects/teamy.vue
+++ pages/projects/teamy.vue
@@ -13,13 +13,13 @@
         <h2 class="subtitle">Na mi is ez?</h2>
         <p>
             A <a href="https://uwr.hu">Tiszavirág</a> UWR (Underwater rugby) csapatnak szánt alkalmazás, amivel kezdetben trackelni lehet az egy-egy edzésen megjelenő játékosokat, 
-            valamint a tagdíj tartozásaikat, befizetéseiket. 
+            valamint a tagdíj tartozásaikat, befizetéseiket
         </p>
         <p>
-            Regisztráció minden csapatnak lehetséges lesz, akik szeretnék 1 helyen digitálisan managelni, hogy ki lesz és ki volt ott az edzéseken.
+            Regisztráció minden csapatnak lehetséges lesz, akik szeretnék 1 helyen digitálisan managelni, hogy ki lesz és ki volt ott az edzéseken
         </p>
         <p>
-            Későbbiekben csoport üzeneteket valamint edzés értesítőket és hasonlókat is tervezek.
+            Későbbiekben csoport üzeneteket valamint edzés értesítőket és hasonlókat is tervezek bele
         </p>
       </div>
     </div>
