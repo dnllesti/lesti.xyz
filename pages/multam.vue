8f0c987165a5f0c65c08ef4add8262ee182482f7
Lesti Dániel <daniellesti73@gmail.com>
2021-02-16 09:51:01 +0100

    mult

--- pages/multam.vue
+++ pages/multam.vue
@@ -2,43 +2,59 @@
   <main-wrapper>
     <div class="main content">
       <h1>Múltam</h1>
-      <h2>Ez tökre nincs kész még</h2>
+      <h2>Iskola</h2>
       <p>
-        Phasellus lorem lectus, imperdiet et mi at, tristique faucibus nisl.
-        Nulla vel iaculis erat, sed efficitur tellus. Vestibulum auctor magna
-        vel magna semper, sed pulvinar ligula viverra. In commodo ultrices
-        tempor. Maecenas elementum nibh ipsum, ac ultrices nunc euismod vel.
-        Nunc in venenatis ipsum. Cras dapibus dui eget luctus laoreet. Donec
-        sollicitudin, velit at sodales aliquam, dolor purus faucibus ligula, a
-        facilisis erat dui hendrerit sem. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas. In condimentum
-        libero ut varius ultricies. Duis vestibulum scelerisque condimentum.
+        Szakgimnáziumba a BMSZC Petrik-be jöttem fel, ahol infó szakot szenvedem
+        végig jelenleg. Jegyeimet tekintve egész elfogadhatóan sose esett le az
+        átlagom 4.8 alá, most félévem is mondjuk 5.0 lett, viszont szerencsémre
+        relatíve nagyon kevés készüléssel tudom hozni ezt a szintet, úgyhogy van
+        / inkább volt kis plussz energiám a saját kis életem managelésére. Így
+        lettem például valahogy tagozatvezető-helyettes egy vegyész
+        szakszervezetben vagy így tudtam el járkálni hetente az ELTE-re pár
+        fizika előadásra.
       </p>
       <hr />
-      <h2>Eskü nem Somogyiék</h2>
+      <h2>Munkahelyek</h2>
       <p>
-        Phasellus lorem lectus, imperdiet et mi at, tristique faucibus nisl.
-        Nulla vel iaculis erat, sed efficitur tellus. Vestibulum auctor magna
-        vel magna semper, sed pulvinar ligula viverra. In commodo ultrices
-        tempor. Maecenas elementum nibh ipsum, ac ultrices nunc euismod vel.
-        Nunc in venenatis ipsum. Cras dapibus dui eget luctus laoreet. Donec
-        sollicitudin, velit at sodales aliquam, dolor purus faucibus ligula, a
-        facilisis erat dui hendrerit sem. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas. In condimentum
-        libero ut varius ultricies. Duis vestibulum scelerisque condimentum.
+        Mint minden jó középiskolás egy Mekiben kezdtem a karrierem talán 3
+        évvel ezelőtt nyáron, ahol igazából az össz tapasztalatom amit szereztem
+        kimerült abban, hogy most már nagyon királyul tudok itthon wrap-et
+        tekerni és soha többé nem szeretnék hasonló helyen dolgozni. Ezután
+        elkezdtem dolgozni a Zuglói Gyermektáborban Soltvadkerten, ahol
+        általános iskolásokat táboroztatunk évente 2-3-4 héten keresztül,
+        programokat szervezve nekik, ahol igazából mindent csinálunk
+        gyakorlatilag amit csak szükséges, legyen szó hangosításról, vetítésről
+        vagy gyakorlatilag majdnem bármiről ami szóba jöhet. Idén nyáron viszont
+        egy pár fokkal menőbb helyeken is lehetőségem volt munkálkodni,
+        padlófűtést és csőfűtést építettünk ki például egy stadionban és egy
+        brutál ingatlan komplexumban na meg a Városligetben. Ezek mellett egy
+        iroda hálózatát építettük teljesen át, lecserélve a régi kábelezést
+        újra, lefektetve kb 700 m UTP kábelt és felszerelve ~20 fali
+        csatlakozót. Itt főleg kábelcsatornákkal legóztam sokat valamint a
+        csatlakozóknak jó részét én szereltem fel. Itt tetszett meg az a
+        gondolat, hogy akár még dolgozhatnék is aktívan ilyenekkel, így jött az
+        ötlet, hogy nekiállok keresgélni. Itt tartunk most. 😇
       </p>
       <hr />
-      <h2>Legyen már valami más is</h2>
+      <h2>Nem közoktatás, nemis munkahely de legalább ingyen</h2>
       <p>
-        Phasellus lorem lectus, imperdiet et mi at, tristique faucibus nisl.
-        Nulla vel iaculis erat, sed efficitur tellus. Vestibulum auctor magna
-        vel magna semper, sed pulvinar ligula viverra. In commodo ultrices
-        tempor. Maecenas elementum nibh ipsum, ac ultrices nunc euismod vel.
-        Nunc in venenatis ipsum. Cras dapibus dui eget luctus laoreet. Donec
-        sollicitudin, velit at sodales aliquam, dolor purus faucibus ligula, a
-        facilisis erat dui hendrerit sem. Pellentesque habitant morbi tristique
-        senectus et netus et malesuada fames ac turpis egestas. In condimentum
-        libero ut varius ultricies. Duis vestibulum scelerisque condimentum.
+        Az idők során elkerültem 1-2 furcsa helyre, többek között a már említett
+        vegyész szakszervezethez, ahol valamilyen varázslatos (amúgy teljesen
+        érthető) okból ifjúsági tagozatvezető-helyettes lettem kemény 17 évesen
+        úgy, hogy a második legfiatalabb személy 25 volt, míg a harmadik 30. Itt
+        aktívan próbáltuk felturbózni a tagozatunkat, viszont ez bedőlt az
+        enyhén brutális bürokrácia miatt sajnos, de azért részt vettem 1-2
+        nemzetközi találkozón és továbbképzésen, ahol szinkrontolmácsokkal
+        felfegyverkezve próbáltuk megváltani a világot, több kevesebb liter kávé
+        elfogyasztása mellett természetesen. Ezek mellett jártam heti 1x az
+        ELTE-re csupán önkéntes alapon <a href="http://atomcsill.elte.hu/NEW/">Az atomoktól a csillagokig</a> 
+        rendezvénysorozatra, ahol mindenről tartanak előadást ami
+        természettudomány. Itt ismerkedtem meg egy kutató doktorral, aki a
+        KFKI-ban kutat tanítás mellett és vezet egy (jelenleg COVID miatt
+        felfüggesztett) diák kutató csoportot, aminek tagja lehettem kemény 1
+        napig, még le nem záratták jóformán a teljes Központi Fizikai Kutató
+        Intézetet a vírus miatt. Az utóbbi 2-t nagyon szeretném tovább
+        folytatni, amint lehetőségem lesz rá, hisz elképesztően élvezem őket.
       </p>
     </div>
   </main-wrapper>
@@ -53,11 +69,11 @@ h2 {
   color: #666;
 }
 .main {
-    padding: 5vw 5vw;
-    @media (max-width: 959px) {
+  padding: 5vw 5vw;
+  @media (max-width: 959px) {
     margin: 10vh 2vw;
-    p{
-    font-size: 0.8rem;
+    p {
+      font-size: 0.8rem;
     }
   }
 }
